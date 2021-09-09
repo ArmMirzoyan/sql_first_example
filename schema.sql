2c99dd7d46f543b70c99a834c643f3ff59703f70
Aram <60153735+ArmMirz@users.noreply.github.com>
2021-09-09 07:01:26 +0400

    maded some improvement

--- schema.sql
+++ schema.sql
@@ -1,26 +1,36 @@
-CREATE DATABASE item_groups;
-CREATE TABLE group_repository
+CREATE DATABASE MYSTERIOUS_DATABASE;
+CREATE TABLE "group"
 (
-    group_id   bigint,
-    group_name varchar(255) NOT NULL,
-    PRIMARY KEY (group_id)
+    id     INT PRIMARY KEY,
+    name   varchar(255) NOT NULL,
+    parent INT REFERENCES "group" (id)
 );
-CREATE TABLE item_repository
+CREATE TABLE configuration
 (
-    item_id         bigint,
-    item_base_price MONEY,
-    item_name       varchar(255) NOT NULL,
-    item_imageUrl   varchar,
-    PRIMARY KEY (item_id)
+    id         serial PRIMARY KEY,
+    Resolution varchar(3) NOT NULL CHECK (Resolution IN ('HD', 'FHD', '_4K'))
 );
-CREATE TABLE item_groups_relation
+CREATE TABLE item
 (
-    group_id bigint,
-    item_id  bigint,
-    CONSTRAINT id_item FOREIGN KEY (group_id) References item_repository(item_id),
-    CONSTRAINT id_group FOREIGN KEY (item_id) References group_repository(group_id)
+    id               INT PRIMARY KEY,
+    base_price       MONEY        NOT NULL,
+    currency         varchar(15)  NOT NULL,
+    name             varchar(255) NOT NULL,
+    imageUrl         varchar(255),
+    complexity       DOUBLE PRECISION,
+    configuration_id INT REFERENCES configuration (id),
+    parent_group     INT REFERENCES "group" (id)
 );
-
-DROP TABLE group_repository;
-DROP TABLE item_repository;
-DROP DATABASE item_groups;
+CREATE TABLE items_groups
+(
+    item_id  INT REFERENCES item (id),
+    group_id INT REFERENCES "group" (id),
+    id       INT PRIMARY KEY,
+    CONSTRAINT item FOREIGN KEY (group_id) References item (id),
+    CONSTRAINT "group" FOREIGN KEY (item_id) References "group" (id)
+);
+DROP DATABASE MYSTERIOUS_DATABASE;
+DROP TABLE items_groups;
+DROP TABLE "group";
+DROP TABLE item;
+DROP TABLE configuration;
